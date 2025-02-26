f070775b655a1dccf4d55a88e44a4666ccc39df1
freedownloadhere <freedownloadhere13@gmail.com>
2025-02-26 19:56:44 +0200

    intermediary commit

--- src/main/kotlin/com/github/freedownloadhere/blocknodes2/gui/interfaces/IParentVariadic.kt
+++ src/main/kotlin/com/github/freedownloadhere/blocknodes2/gui/interfaces/IParentVariadic.kt
@@ -1,12 +1,10 @@
 package com.github.freedownloadhere.blocknodes2.gui.interfaces
 
 import com.github.freedownloadhere.blocknodes2.gui.Gui
-import com.github.freedownloadhere.blocknodes2.gui.utils.ParentChildLayout
 
 interface IParentVariadic : IParent {
     override val children: MutableList<Gui>
     fun addChild(child : Gui) {
         children.add(child)
-        ParentChildLayout.apply(this as Gui, child)
     }
 }
